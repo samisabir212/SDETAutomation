d4f61d8ae266ea64352b46d0f634ae6de4ec1b3b
Sami Sabir-Idrissi <admin@techteksolution.com>
2019-05-25 11:48:29 -0400

    Updated auto complete selenium code

--- src/test/java/Selenium/HowToAutoCompleteTextFieldSelectDropDown.java
+++ src/test/java/Selenium/HowToAutoCompleteTextFieldSelectDropDown.java
@@ -1,29 +1,95 @@
 package Selenium;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class HowToAutoCompleteTextFieldSelectDropDown {
 
 	//free Online resource : http://total-qa.com/auto-suggestion-auto-complete-text-box-visibilityofallelementslocatedby-example/
 	
-	
-	public static void main(String[] args) throws InterruptedException {
-		
-		System.setProperty("webdriver.chrome.driver", Constants.chromeDriverPath);
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.expedia.com/");
+	WebDriver driver;
+	WebDriverWait wait;
+
+	String URL = "http://jqueryui.com/autocomplete/";
+	private By frameLocator = By.className("demo-frame");
+	private By tagText = By.id("tags");
+
+	@BeforeClass
+	public void Setup() {
+		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		
+		wait = new WebDriverWait(driver, 5);
+	}
+
+	@Test
+	public void rightClickTest() {
+		driver.navigate().to(URL);
+		WebElement frameElement=driver.findElement(frameLocator);
+		driver.switchTo().frame(frameElement);
+		wait.until(ExpectedConditions.presenceOfElementLocated(tagText));
+		WebElement textBoxElement = driver.findElement(tagText);
+		textBoxElement.sendKeys("a");
+		selectOptionWithText("Java");
+		//selectOptionWithIndex(2);
 		
 	}
 	
+	public void selectOptionWithText(String textToSelect) {
+		try {
+			WebElement autoOptions = driver.findElement(By.id("ui-id-1"));
+			wait.until(ExpectedConditions.visibilityOf(autoOptions));
+
+			List<WebElement> optionsToSelect = autoOptions.findElements(By.tagName("li"));
+			for(WebElement option : optionsToSelect){
+		        if(option.getText().equals(textToSelect)) {
+		        	System.out.println("Trying to select: "+textToSelect);
+		            option.click();
+		            break;
+		        }
+		    }
+			
+		} catch (NoSuchElementException e) {
+			System.out.println(e.getStackTrace());
+		}
+		catch (Exception e) {
+			System.out.println(e.getStackTrace());
+		}
+	}
+	
+public void selectOptionWithIndex(int indexToSelect) {
+		
+		try {
+			WebElement autoOptions = driver.findElement(By.id("ui-id-1"));
+			wait.until(ExpectedConditions.visibilityOf(autoOptions));
+
+			List<WebElement> optionsToSelect = autoOptions.findElements(By.tagName("li"));
+		        if(indexToSelect<=optionsToSelect.size()) {
+		        	System.out.println("Trying to select based on index: "+indexToSelect);
+		           optionsToSelect.get(indexToSelect).click();
+		        }
+		} 		
+		catch (NoSuchElementException e) {
+			System.out.println(e.getStackTrace());
+		}
+		catch (Exception e) {
+			System.out.println(e.getStackTrace());
+		}
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
 	
 }
